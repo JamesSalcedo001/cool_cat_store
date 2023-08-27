eeac83f18cb46a1692d97888b9ada335e74cc9ad
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-08-27 17:01:20 -0500

    wrapped Sessions controller in api namespace

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,21 +1,24 @@
-class SessionsController < ApplicationController
+module Api
+    class SessionsController < ApplicationController
 
-    skip_before_action :authorize, only: :create
-
-    def create
-        user = User.find_by(username: params[:username])
-        if user&.authenticate(params[:password])
-            session[:user_id] = user.id
-            render json: user, status: :ok
-        else
-            render json: { errors: ["Please enter existing username and password"]}, status: :unauthorized
-        end
-    end
-
-
-    def destroy
-        sessiom.delete :user_id
-        head :no_content
-    end
+        skip_before_action :authorize, only: :create
     
+        def create
+            user = User.find_by(username: params[:username])
+            if user&.authenticate(params[:password])
+                session[:user_id] = user.id
+                render json: user, status: :ok
+            else
+                render json: { errors: ["Please enter existing username and password"]}, status: :unauthorized
+            end
+        end
+    
+    
+        def destroy
+            sessiom.delete :user_id
+            head :no_content
+        end
+        
+    end
 end
+
