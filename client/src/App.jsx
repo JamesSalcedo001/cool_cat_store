ea0f87067c4db2222b81d99a41e1d7e0beecfe1a
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-08-27 02:55:55 -0500

    created the header component for navigation and for logging out a user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Route, Routes } from "react-router-dom";
 import { useEffect } from 'react';
+import { useDispatch } from "react-redux"
 import ProductsList from './ProductsList';
 import Cancel from './Cancel';
 import Success from './Success';
@@ -8,15 +9,22 @@ import Success from './Success';
 
 
 function App() {
+  const dispatch = useDispatch()
 
   useEffect(() => {
     fetch("/api/me")
     .then((res) => res.json())
-    .then((data) => console.log(data))
-  },[])
+    .then((data) => {
+      if (data.username) {
+        dispatch(login(data))
+        console.log(data)
+      }
+    })
+  },[dispatch])
   
   return (
     <div className="App">
+      <Header/>
         <Routes>
           <Route path="/products_list" element={<ProductsList/>} />
           <Route path="/cancel" element={<Cancel/>} />
--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,6 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "./slices/userSlice"
+import { useNavigate, NavLink } from "react-router-dom";
+
 function Header() {
+    const loggedIn = useSelector(state => state.user.loggedIn)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+
+    const logOutUser = () => {
+        fetch("/api/logout", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(() => {
+            dispatch(logout())
+            navigate("/")
+        })
+    }
+
+
     return (
-        <h1></h1>
+        <div className="headerDiv">
+            { loggedIn ? (
+                <>
+                    <button onClick={logOutUser}>Log Out!</button>
+                    <NavLink to="/">Profile!</NavLink>
+                    <NavLink to="/products_list">Products</NavLink>
+                </>
+                ) : (
+                    <h3>welcome</h3>
+             )}
+        </div>
     )
 }
 
