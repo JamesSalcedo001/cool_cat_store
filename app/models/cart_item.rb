714d4924e010edaedf64f8cd487e57536cdeb041
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-08-30 18:27:58 -0500

    added a before save method, adjusted the quantity validation and now if a cartitem's quantity is zero it's removed from cart

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,7 +2,13 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
-  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  
+  before_save :remove_if_zero
+
+  def remove_if_zero
+    self.destroy if quantity.zero?
+  end
+
+
 end
