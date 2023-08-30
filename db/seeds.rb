91f6fe265ecd66fe68f3285edca628ea6dc35529
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-08-30 16:47:34 -0500

    seed data for image attribute fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Product.destroy_all
+
 Product.create!(title: "Sunglasses", description: "Cool Shades", stripe_id: "price_1NiXnrIBnVrhy9EW9a3soJOj", image: "https://m.media-amazon.com/images/I/51x7nqTd3vS._AC_SL1500_.jpg", price: 999)
 
-Product.create!(title: "Cool Hat", description: "Stylish black dad hat!", stripe_id: "price_1NiuAqIBnVrhy9EWsHBKQl3m", image: "https://n.nordstrom
-    media.com/id/sr3/943c7f7d-ddb2-4ae5-a7c8-0701a35873cc.jpeg?h=365&w=240&dpr=2", price: 999)
+Product.create!(title: "Cool Hat", description: "Stylish black dad hat!", stripe_id: "price_1NiuAqIBnVrhy9EWsHBKQl3m", image: "https://n.nordstrommedia.com/id/sr3/943c7f7d-ddb2-4ae5-a7c8-0701a35873cc.jpeg?h=365&w=240&dpr=2", price: 999)
